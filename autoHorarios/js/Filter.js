c037ef9e6394e403013b40e9b57282bb8974ff74
ramagg <rg1998@hotmail.es>
2018-12-02 21:57:28 -0300

    Filter done

--- autoHorarios/js/Filter.js
+++ autoHorarios/js/Filter.js
@@ -1,14 +1,57 @@
 // Filter function 
-function disc(item){
-  console.log(item)
-  return item[0].Dia[0]
+
+
+function deter(h1,h2){
+  // Determina si dos horarios pueden estar juntos
+  let min = h1
+  let max = h2
+
+  if (min > max) {
+    min = h2
+    max = h1
+  } 
+  return (min + 1.5) < max ;
 }
 
-const result = comb.filter(disc);
+function test(arr){
+  // Evalua si un dia no tiene horarios solapados
+  let i = 0
+  let j = 0
+  Cond = true
+  while ((i < arr.length - 1 ) && (Cond)){
+    j = i + 1
+    while ((j < arr.length) && (Cond)){
+      Cond = deter(arr[i], arr[j]);
+      j++
+    }
+    i++
+  }
+  return Cond
+}
+
+function DISC(item){
+  // Filtra las posibilidades con horarios solapados
+  let i = 0
+  let eval = true
+
+  while ((i < item[0].Dia.length) && (eval)) {
+    let temp = []
+    for (let j = 0; j < item.length; j++) {
+      if (item[j].Dia[i]){
+        temp.push(item[j].Horario[i])
+      }
+    }
+    eval = test(temp);
+    i++
+  }
+  if (eval){
+    return item
+  }
+}
+
+const result = comb.filter(DISC);
 
 const parentDiv = document.querySelector("#table");
-
-
 result.forEach((item)=> {
   let element = document.createElement("div");
   let p = ``;
@@ -23,9 +66,6 @@ result.forEach((item)=> {
       }
     })
   })
-  
-  
-
   // console.log(item)
   element.innerHTML = p;
   element.setAttribute('class','comb')
